1aca6541bfaa76b3fa38d2eba752ce4ce7ba69eb
Christian Dürr <christian@juergen-duerr.de>
2015-10-12 02:11:02 +0200

    Fixed issues so it would run on Linux machines

--- PrintMail.py
+++ PrintMail.py
@@ -1,17 +1,19 @@
 import platform
 
 # Windows print imports
-import tempfile
-import win32api
-import win32print
+if platform.system() == 'Windows':
+	import tempfile
+	import win32api
+	import win32print
 
 # Linux print imports
-import subprocess
+if platform.system() == 'Linux':
+	import subprocess
 
 def printmail(subject, body):
-    print("Printing \"" + subject + "\"...")
+    print "Printing \"" + subject + "\"..."
     printcontent = subject + "\n\n\n" + body
-    if platform.system() is 'Windows':
+    if platform.system() == 'Windows':
         # Print mail on windows
         filename = tempfile.mktemp(".txt")
         open (filename, "w").write (printcontent)
@@ -23,7 +25,7 @@ def printmail(subject, body):
         ".",
         0
         )
-    elif platform.system() is 'Linux':
+    if platform.system() == "Linux":
         # Print mail on Linux
         lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
         lpr.stdin.write(printcontent)
